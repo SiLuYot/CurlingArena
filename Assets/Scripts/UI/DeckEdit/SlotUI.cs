a5c4f238be5fac89f9ba903cce3ac451655526cd
SiLuYot <38523915+SiLuYot@users.noreply.github.com>
2021-01-08 19:19:15 +0900

    슬롯 랭크 별 테두리 작업

--- Assets/Scripts/UI/DeckEdit/SlotUI.cs
+++ Assets/Scripts/UI/DeckEdit/SlotUI.cs
@@ -6,6 +6,7 @@ public class SlotUI : MonoBehaviour
     public UILabel slotName;
     public GameObject selectHighlightRoot;
     public GameObject blockRoot;
+    public UISprite rankSprite;
 
     public int Index { get; private set; }
     public CharacterData Data { get; private set; }
@@ -28,6 +29,37 @@ public class SlotUI : MonoBehaviour
 
         this.isPress = false;
         this.pressTime = 0;
+
+        if (rankSprite != null)
+        {
+            Color color = Color.white;
+
+            if (Data != null)
+            {
+                switch (Data.rarityData.id)
+                {
+                    //case 0:
+                    //    break;
+                    case 1:
+                        color = Color.green;
+                        break;
+                    case 2:
+                        color = new Color(1f, 0, 1f);
+                        break;
+                    case 3:
+                        color = Color.red;
+                        break;
+                    case 4:
+                        color = new Color(1f, 0.498f, 0);
+                        break;
+                    case 5:
+                        color = Color.yellow;
+                        break;
+                }
+            }
+
+            rankSprite.color = color;
+        }
 
         ActiveHighlight(false);
         ActiveBlock(false);
